6b7fd05b0b29284048aca82b508f48f8c562a4b0
taelsher <taelsher@users.noreply.github.com>
2024-04-21 22:34:34 -0600

    chore: add vite config

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import copy from "rollup-plugin-copy";
+
+export default defineConfig({
+  plugins: [
+    copy({
+      targets: [
+        { src: "Projects/*", dest: "dist/Projects" },
+        { src: "Media_1/*", dest: "dist/Media_1" },
+      ],
+      hook: "writeBundle", // Ensures files are copied after the bundle is written
+    }),
+  ],
+  build: {
+    // Your other build configurations here
+    outDir: "dist",
+  },
+});
